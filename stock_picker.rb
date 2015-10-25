e1104e2864b5f74524a714169f049502365953cf
BoDonkey <robert.means1969@gmail.com>
2015-10-25 07:50:35 -0400

    Finish Project 2

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,23 @@
-def stock_picker(price_array)
-	init_sell_price = price_array.each_with_index.max
-	init_buy_price = price_array.each_with_index.min
-	if init_sell_price[1] > init_buy_price[1]
-		puts "Buy at $" + init_buy_price[0].to_s + " on day" + init_buy_price[1].to_s
-		puts "Sell at $" + init_sell_price[0].to_s + " on day" + init_sell_price[1].to_s
-		profit = init_sell_price[0]-init_buy_price[0]
-		period = init_sell_price[1]-init_buy_price[1]
-		puts "Total profit: $" + profit.to_s + " in " + period.to_s + " days."
+def stock_picker(prices)
+	current_best_profit = prices[1] - prices.first
+	current_best_dates = [0,1]
+	prices.each_with_index do |buy_price, buy_day|
+		(buy_day...prices.length).each_with_index do |sell_day|
+			sell_price = prices[sell_day]
+			if (sell_price - buy_price) > current_best_profit
+				current_best_profit = sell_price - buy_price
+				current_best_dates = [buy_day, sell_day]
+			end
+		end
 	end
+	if current_best_profit <= 0
+		return "No trades possible"
+	end
+	buy_day = current_best_dates[0]
+  sell_day = current_best_dates[1]
+  puts "Buy at $" + prices[buy_day].to_s + " on day " + buy_day.to_s
+    puts "Sell at $" + prices[sell_day].to_s + " on day " + sell_day.to_s
+    period = sell_day - buy_day
+    puts "Total profit: $" + current_best_profit.to_s + " in " + period.to_s + " days."
+
 end
-
-
-
-
